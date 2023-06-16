717bbb5c8bb2db7ea915c8798786ed45d046ef1a
Matthew Fernandez <matthew9erfernandez@gmail.com>
2023-06-16 00:41:22 -0700

    Updated branch

--- meteorite_landings.sql
+++ meteorite_landings.sql
@@ -1,20 +1,75 @@
-drop table if exists landing;
-CREATE TABLE meteorite_landing (
-	name varchar (100) NOT NULL,
+drop table if exists us_aqi;
+CREATE TABLE us_aqi (
 	id int NOT NULL,
-	nametype varchar (30) NOT NULL,
-	recclass varchar (100) NOT NULL,
-	mass_g double precision,
-	fall varchar (10) NOT NULL,
-	year int,
-	reclat double precision,
-	reclong double precision,
-	GoeLocation varchar (100),
+	CBSA_Code int NOT NULL,
+	Date date NOT NULL,
+	AQI int NOT NULL,
+	Category varchar (100) NOT NULL,
+	Defining_Parameter varchar(50) NOT NULL,
+	Number_of_Sites_Reporting int NOT NULL,
+	city_ascii varchar(25) NOT NULL,
+	state_id varchar(2) NOT NULL,
+	state_name varchar(25) NOT NULL,
+	lat double precision NOT NULL,
+	lng double precision NOT NULL,
+	population double precision NOT NULL,
+	density double precision NOT NULL,
+	timezone varchar(100),
 	primary key (id)
+
 );
 
-copy meteorite_landing from 
-'/private/tmp/Meteorite_Landings.csv' 
+
+copy us_aqi from 
+'/private/tmp/US_AQI.csv' 
 csv header delimiter ',';
 
-select * from meteorite_landing;
+select * from us_aqi;
+
+drop table if exists ca_wildfires;
+CREATE TABLE ca_wildfires (
+	Acres_burned int,
+	Active Boolean,
+	AdminUnit varchar(300) NOT NULL,
+	AirTankers int,
+	ArchiveYear varchar(100),
+	calFireIncident varchar(5) NOT NULL,
+	CanonicaUrl varchar(500),
+	ConditionStatement varchar(10000),
+	ControlStatement varchar(1000),
+	Counties varchar(50),
+	CountyIds varchar(200),
+	CrewsInvolved int, 
+	Dozers int,
+	Engines int,
+	Extinguished date,
+	Fatalities int,
+	Featured varchar (8),
+	Final varchar(5),
+	FuelType varchar(50),
+	Helicopters int,
+	Injuries int,
+	Latiitude double precision,
+	Location varchar(200),
+	Longitude double precision,
+	MajorIncident boolean,
+	Name varchar(200),
+	PercentContained int,
+	PersonnelInvolved int,
+	Public varchar(5),
+	SearchDescription varchar(1000),
+	SearchKeywords varchar(1000),
+	Started date,
+	Status varchar(15),
+	StructuresDamaged int,
+	StructuresDestroyed int,
+	StructuresEvacuated int,
+	StructuresThreatened int,
+	UniqueId varchar(200) NOT NULL,
+	Updated date NOT NULL,
+	WaterTenders int
+);
+
+copy ca_wildfires from
+'/private/tmp/California_Fire_Incidents.csv'
+csv header delimiter ',';
