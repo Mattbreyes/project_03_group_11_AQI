123865e58425e73662435c2bcfbbe1c3cd55652c
Nazelik <77356570+Nazelik@users.noreply.github.com>
2023-06-25 18:01:57 -0700

    Updated AirQuality.js file to work for user-selected date

--- static/js/AirQuality.js
+++ static/js/AirQuality.js
@@ -1,82 +1,15 @@
+// This is the first load of the web page, with initial date value.
+let baseMap = createBaseMap();
+updateMap(baseMap);
 
-// Load the GeoJSON data.
-var geoData = "/1981-01-01"
-var geojson;
-var xhr = new XMLHttpRequest();
-xhr.open('GET', '/1981-01-01', true);
-xhr.onreadystatechange = function() {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    var geoData = JSON.parse(xhr.responseText);
-    createFeatures(geoData.features);   
-  }
-};
-xhr.send();
-
-// --------------------------------------------------------------------------------------------------------------------
-function createFeatures(airQualityData) {
-
-  // Define a function that we want to run once for each feature in the features array.
-  // Give each feature a popup that describes the air quality, city name and population of the coordinate.
-  function onEachFeature(feature, layer) {
-    layer.bindPopup(`<h2>${feature.properties.aqi}</h2><h4>City:  ${feature.properties.city_ascii}</h4><h4>Population:  ${feature.properties.population}</h4>`);
-
-  }
-
-  function ChooseSize(population) {
-    return population / 400000;
-  }
-  
-
-  function ChooseColor(aqi) {
-    if (aqi >= 300) {
-        color = "Maroon";
-    }
-    else if (aqi >= 200) {
-        color = "Purple";
-    }
-    else if (aqi >= 150) {
-        color = "Red";
-    }
-    else if (aqi >= 100) {
-        color = "Orange";
-        
-    }
-    else if (aqi >= 50) {
-        color = "Yellow";
-    }
-    else if (aqi >= 0) {
-        color = "Green";
-    }
-    console.log("Population: " + aqi + " Color: " + color);
-   
-    return color;
-  };
-
-
-  // Create a GeoJSON layer that contains the features array on the airQualityData object.
-  // Run the onEachFeature function once for each piece of data in the array.
-  let airQuality = L.geoJSON(airQualityData, {
-    onEachFeature: onEachFeature,
-    pointToLayer: function (feature, latlng) {
-      var geojsonMarkerOptions = {
-        radius: ChooseSize(feature.properties.population),
-        fillColor: ChooseColor(feature.properties.aqi),
-        color: "#000",
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.8
-      };
-      return L.circleMarker(latlng, geojsonMarkerOptions);
-  }
-  });
-
-
-  // Send the earthquakes layer to the createMap function/
-  createMap(airQuality);
-}
+// When clicked on "Go" button, the selected date will be passed to the updateMap function
+// to generate new overlay map with updated air quality data.
+document.getElementById('selected_date_button').addEventListener('click', function() {
+  updateMap(baseMap);
+})
 
 // ---------------------------------------------------------------------------------------------------------------------
-function createMap(airQuality) {
+function createBaseMap() {
 
   // Create the base layers.
  
@@ -91,81 +24,100 @@ function createMap(airQuality) {
     "Grayscale": grayscale, 
   };
 
-  // Create an overlay object to hold our overlay.
-  let overlayMaps = {
-    AirQuality: airQuality
-  };
-
   // Create our map, giving it the streetmap and earthquakes layers to display on load.
   let myMap = L.map("map", {
     center: [
       37.09, -95.71
     ],
     zoom: 5,
-    layers: [grayscale, airQuality]
+    layers: [grayscale]
   });
 
-
- /*
-  function ChooseColor(aqi) {
-    if (aqi >= 300) {
-        color = "Maroon";
-    }
-    else if (aqi >= 200) {
-        color = "Purple";
-    }
-    else if (aqi >= 150) {
-        color = "Red";
-    }
-    else if (aqi >= 100) {
-        color = "Orange";
-        
-    }
-    else if (aqi >= 50) {
-        color = "Yellow";
-    }
-    else if (aqi >= 0) {
-        color = "Green";
-    }
-    console.log("Population: " + aqi + " Color: " + color);
-   
-    return color;
-  };
-
- 
-  
-  let minOfAqiIntervals = [0, 51, 101, 151, 201, 301]
-  let colorsList = [];
-  for (let i = 0; i < 6; i++) {
-    colorsList.push(ChooseColor(minOfAqiIntervals[i]));
-  }
-  console.log(colorsList)
-
-  // create legend
-  var legend = L.control({
-    position: "bottomright"
-  });
-
-  
-  legend.onAdd = function(map) {
-    let div = L.DomUtil.create('div', 'legend');
-    let intervals = ['0 - 50', '51 - 100', '101 - 150', '151 - 200', '201 - 300', '301 +'];
-    
-    // Loop through the intervals and colorsList to create the legend items
-    for (let i = 0; i < intervals.length; i++) {
-      let interval = intervals[i];
-      let color = colorsList[i];
-      
-      div.innerHTML +=
-        '<div><i style="background:' + color + '"></i>' +
-        interval + '</div>';
-    }
-    
-    return div;
-  };
-  
-
-  // add legend to the map.
-  legend.addTo(myMap);
-  */
+  return myMap;
 }
+
+// ---------------------------------------------------------------------------------------------------------------------
+function updateMap(myMap) {
+  
+  // Get the selected date from the dropdown menu
+  var selectedDate = document.getElementById('inputDate').value;
+
+  // Construct the URL with the selected date
+  var url = '/' + selectedDate;
+  
+  // Send a request to the Flask app with the selected date
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var geoData = JSON.parse(xhr.responseText);
+
+
+
+      // Define a function that we want to run once for each feature in the features array.
+      // Give each feature a popup that describes the air quality, city name and population of the coordinate.
+      function onEachFeature(feature, layer) {
+        layer.bindPopup(`<h2>${feature.properties.aqi}</h2><h4>City:  ${feature.properties.city_ascii}</h4><h4>Population:  ${feature.properties.population}</h4>`);
+
+      }
+
+      function ChooseSize(population) {
+        return population / 400000;
+      }
+      
+
+      function ChooseColor(aqi) {
+        if (aqi >= 300) {
+            color = "Maroon";
+        }
+        else if (aqi >= 200) {
+            color = "Purple";
+        }
+        else if (aqi >= 150) {
+            color = "Red";
+        }
+        else if (aqi >= 100) {
+            color = "Orange";
+            
+        }
+        else if (aqi >= 50) {
+            color = "Yellow";
+        }
+        else if (aqi >= 0) {
+            color = "Green";
+        }
+        //console.log("Population: " + aqi + " Color: " + color);
+      
+        return color;
+      };
+
+
+      // Create a GeoJSON layer that contains the features array on the airQualityData object.
+      // Run the onEachFeature function once for each piece of data in the array.
+      let airQuality = L.geoJSON(geoData.features, {
+        onEachFeature: onEachFeature,
+        pointToLayer: function (feature, latlng) {
+          var geojsonMarkerOptions = {
+            radius: ChooseSize(feature.properties.population),
+            fillColor: ChooseColor(feature.properties.aqi),
+            color: "#000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+          };
+          return L.circleMarker(latlng, geojsonMarkerOptions);
+      }
+      });
+
+      // Create an overlay object to hold our overlay.
+      let overlayMaps = {
+        AirQuality: airQuality
+      };
+      airQuality.addTo(myMap);
+
+    }
+  };
+  xhr.send();
+
+}
+
