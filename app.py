c1066a60048edc78df6b3e8224e3737f05a4ce20
Matthew Fernandez <matthew9erfernandez@gmail.com>
2023-06-27 01:57:16 -0700

    Updated version

--- app.py
+++ app.py
@@ -36,12 +36,10 @@ def index():
 def visual_2(query):
     cur.execute(query)
     rows = cur.fetchall()
-    # print({*rows})
     array_of_rows_visual_2 = []
     dict_of_row_visual_2 = {}
 
     for row in rows:
-        # print(row, flush = True)
         dict_of_row_visual_2 = {}
         dict_of_row_visual_2["aqi"] = row[0]
         dict_of_row_visual_2["city_ascii"] = row[1]
@@ -51,7 +49,27 @@ def visual_2(query):
         # Append the current dictionary to the list
         array_of_rows_visual_2.append(dict_of_row_visual_2)
  
-    print('Type: ',type(array_of_rows_visual_2))
+    print('Type Visual1: ',type(array_of_rows_visual_2))
+    return (array_of_rows_visual_2)
+
+# Helper Fn to get the visual for our box plot
+def visual_3(query):
+    cur.execute(query)
+    rows = cur.fetchall()
+    array_of_rows_visual_2 = []
+    dict_of_row_visual_2 = {}
+
+    for row in rows:
+        # print(row, flush = True)
+        dict_of_row_visual_2 = {}
+        dict_of_row_visual_2["Date"] = row[0]
+        dict_of_row_visual_2["aqi"] = row[1]
+        dict_of_row_visual_2["state_id"] = row[2]
+        dict_of_row_visual_2["city_ascii"] = row[3]
+        # Append the current dictionary to the list
+        array_of_rows_visual_2.append(dict_of_row_visual_2)
+ 
+    print('Type Visual2: ',type(array_of_rows_visual_2))
     return (array_of_rows_visual_2)
 
 # End point for barplot
@@ -140,9 +158,150 @@ def start(selected_date='2000-01-01'):
         return dict_of_GeoJSON #, visual_2(query_visual_2) )
     return {"ERROR": "DATE NEEDED"}
 
-@airApp.route('/test/stableDiff')
-def stableDiff():
-    return jsonify(1)
+
+@airApp.route('/line/<selected_date>')
+def linePlot(selected_date='2000-01-01', state_id='CA'):
+    if "favicon" not in selected_date:
+
+        print(selected_date)
+        add_year_array = selected_date.split("-")[0]
+        selected_date_plus1yr = int(add_year_array)
+
+
+
+        # query_visual_2 = f"commit;SELECT Date, aqi, state_id, city_ascii FROM us_aqi WHERE Date = '{selected_date}' ORDER BY Date limit 5;"
+        query = f"""commit;SELECT Date, aqi, state_id, city_ascii 
+                    FROM us_aqi WHERE Date = '{selected_date}' 
+                    AND state_id like '{state_id}' 
+                    ORDER BY aqi DESC limit 5;"""
+        json_data = visual_3(query)
+        json_data
+        print(json_data,'\nLinePlot  ',query)
+        # cur.close()
+        return jsonify(json_data)
+    return {"ERROR": "DATE NEEDED"}
+
+# Logic to get the selected date plus 1 year from db
+@airApp.route('/lineplot/<selected_date>')
+def linePlot_yr1(selected_date='2000-01-01', state_id='CA'):
+    if "favicon" not in selected_date:
+
+        print(selected_date)
+        add_year_array = selected_date.split("-")
+        print('YEAR->',add_year_array)
+        selected_date_plus1yr = int(add_year_array[0])+1
+        selected_date = str(selected_date_plus1yr) +'-'+ add_year_array[1] +'-'+ add_year_array[2]
+        print('linePlot_yr2',selected_date)
+
+        # query_visual_2 = f"commit;SELECT Date, aqi, state_id, city_ascii FROM us_aqi WHERE Date = '{selected_date}' ORDER BY Date limit 5;"
+        query = f"""commit;SELECT Date, aqi, state_id, city_ascii 
+                    FROM us_aqi WHERE Date = '{selected_date}' 
+                    AND state_id like '{state_id}' 
+                    ORDER BY aqi DESC;"""
+        json_data = visual_3(query)
+        json_data
+        print('LinePlot 1 YEAR',query)
+        # cur.close()
+        return jsonify(json_data)
+    return {"ERROR": "DATE NEEDED"}
+
+
+# Logic to get the selected date plus 2 year from db
+@airApp.route('/lineplot2/<selected_date>')
+def linePlot_yr2(selected_date='2000-01-01', state_id='CA'):
+    if "favicon" not in selected_date:
+
+        print(selected_date)
+        add_year_array = selected_date.split("-")
+        print('YEAR->',add_year_array)
+        selected_date_plus2yr = int(add_year_array[0])+2
+        selected_date = str(selected_date_plus2yr) +'-'+ add_year_array[1] +'-'+ add_year_array[2]
+        print('linePlot_yr2',selected_date)
+
+        # query_visual_2 = f"commit;SELECT Date, aqi, state_id, city_ascii FROM us_aqi WHERE Date = '{selected_date}' ORDER BY Date limit 5;"
+        query = f"""commit;SELECT Date, aqi, state_id, city_ascii 
+                    FROM us_aqi WHERE Date = '{selected_date}' 
+                    AND state_id like '{state_id}' 
+                    ORDER BY aqi DESC;"""
+        json_data = visual_3(query)
+        print('LinePlot 2 YEAR  ',query)
+        # cur.close()
+        return jsonify(json_data)
+    return {"ERROR": "DATE NEEDED"}
+
+# Logic to get the selected date plus 3 year from db
+@airApp.route('/lineplot3/<selected_date>')
+def linePlot_yr3(selected_date='2000-01-01', state_id='CA'):
+    if "favicon" not in selected_date:
+
+        print(selected_date)
+        add_year_array = selected_date.split("-")
+        print('YEAR->',add_year_array)
+        selected_date_plus2yr = int(add_year_array[0])+3
+        selected_date = str(selected_date_plus2yr) +'-'+ add_year_array[1] +'-'+ add_year_array[2]
+        print('linePlot_yr2',selected_date)
+
+        # query_visual_2 = f"commit;SELECT Date, aqi, state_id, city_ascii FROM us_aqi WHERE Date = '{selected_date}' ORDER BY Date limit 5;"
+        query = f"""commit;SELECT Date, aqi, state_id, city_ascii 
+                    FROM us_aqi WHERE Date = '{selected_date}' 
+                    AND state_id like '{state_id}' 
+                    ORDER BY aqi DESC;"""
+        json_data = visual_3(query)
+        print('LinePlot 3 YEAR  ',query)
+        # cur.close()
+        return jsonify(json_data)
+    return {"ERROR": "DATE NEEDED"}
+
+# Logic to get the selected date plus 4 year from db
+@airApp.route('/lineplot4/<selected_date>')
+def linePlot_yr4(selected_date='2000-01-01', state_id='CA'):
+    if "favicon" not in selected_date:
+
+        print(selected_date)
+        add_year_array = selected_date.split("-")
+        print('YEAR->',add_year_array)
+        selected_date_plus2yr = int(add_year_array[0])+4
+        selected_date = str(selected_date_plus2yr) +'-'+ add_year_array[1] +'-'+ add_year_array[2]
+        print('linePlot_yr2',selected_date)
+
+        # query_visual_2 = f"commit;SELECT Date, aqi, state_id, city_ascii FROM us_aqi WHERE Date = '{selected_date}' ORDER BY Date limit 5;"
+        query = f"""commit;SELECT Date, aqi, state_id, city_ascii 
+                    FROM us_aqi WHERE Date = '{selected_date}' 
+                    AND state_id like '{state_id}' 
+                    ORDER BY aqi DESC;"""
+        json_data = visual_3(query)
+        print('LinePlot 4 YEAR  ',query)
+        # cur.close()
+        return jsonify(json_data)
+    return {"ERROR": "DATE NEEDED"}
+
+# Logic to get the selected date plus 5 year from db
+@airApp.route('/lineplot5/<selected_date>')
+def linePlot_yr5(selected_date='2000-01-01', state_id='CA'):
+    if "favicon" not in selected_date:
+
+        print(selected_date)
+        add_year_array = selected_date.split("-")
+        print('YEAR->',add_year_array)
+        selected_date_plus2yr = int(add_year_array[0])+5
+        selected_date = str(selected_date_plus2yr) +'-'+ add_year_array[1] +'-'+ add_year_array[2]
+        print('linePlot_yr2',selected_date)
+
+        # query_visual_2 = f"commit;SELECT Date, aqi, state_id, city_ascii FROM us_aqi WHERE Date = '{selected_date}' ORDER BY Date limit 5;"
+        query = f"""commit;SELECT Date, aqi, state_id, city_ascii 
+                    FROM us_aqi WHERE Date = '{selected_date}' 
+                    AND state_id like '{state_id}' 
+                    ORDER BY aqi DESC;"""
+        json_data = visual_3(query)
+        print('LinePlot 5 YEAR  ',query)
+        # cur.close()
+        return jsonify(json_data)
+    return {"ERROR": "DATE NEEDED"}
+# @airApp.route('/test/stableDiff')
+# def stableDiff():
+#     if "favicon" not in selected_date:
+#         pass
+#     return jsonify(1)
 
 if __name__ == '__main__':
     airApp.run(debug=True)
